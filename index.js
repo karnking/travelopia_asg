7fc967dba173dbbd41d5b34b48cfccdce299a26a
karnking <karankotai9183@gmail.com>
2023-12-05 15:27:57 +0530

    cypress added

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-const desc = "Armenia is a mountainous nation in West Asia, a former Soviet Republic, and was one of the first Christian nations, embracing Christianity in the fourth century. This rich cultural and natural environment makes it an exciting destination for a wide range of adventure, activity and walking holidays. <br /> <br /> There are many pagan sites to visit as well as UNESCO World Heritage - listed churches, which are–almost without exception–built in stunning locations.Surrounded by forests, perched on the top of dramatic gorges or silhouetted before snow - capped mountains, they draw us to magnificent open spaces filled with a huge variety of trees and wild flowers.On Armenia tours we see wonderful mountains, lakes and waterfalls in the countryside–and relish cooling breezes– while the cafés and restaurants of the country’ s capital, Yerevan, are proof of an emerging economy. Mount Ararat towers majestically over the city, and beautiful scenery is plentiful."
-let state = true
+// The initial description text
+const desc = "Armenia is a mountainous nation in West Asia, a former Soviet Republic, and was one of the first Christian nations, embracing Christianity in the fourth century. This rich cultural and natural environment makes it an exciting destination for a wide range of adventure, activity, and walking holidays. <br /> <br /> There are many pagan sites to visit as well as UNESCO World Heritage - listed churches, which are–almost without exception–built in stunning locations.Surrounded by forests, perched on the top of dramatic gorges or silhouetted before snow - capped mountains, they draw us to magnificent open spaces filled with a huge variety of trees and wild flowers.On Armenia tours we see wonderful mountains, lakes, and waterfalls in the countryside–and relish cooling breezes– while the cafés and restaurants of the country’ s capital, Yerevan, are proof of an emerging economy. Mount Ararat towers majestically over the city, and beautiful scenery is plentiful.";
 
+// Initial state variable for tracking whether "Show More" or "Show Less" is displayed
+let state = true;
+
+// Function to handle button click
 const handleClick = () => {
-    document.querySelector('.description>p').innerText = !state ? desc.substring(0, 201) + '...' : desc
-    document.querySelector('.description>button').innerText = state ? "Show Less":"Show More"
-    document.querySelector('.description>button').innerHTML += state ? " <i class='fa-solid fa-arrow-up'></i>" : " <i class='fa-solid fa-arrow-down'></i>"
-    state = !state
-}
+    // Toggle the displayed text based on the state
+    document.querySelector('.description>p').innerHTML = !state ? desc.substring(0, 201) + '...' : desc;
 
-let w = window.innerWidth
+    // Toggle the button text
+    document.querySelector('.description>button>span').innerText = state ? "Show Less" : "Show More";
+
+    // Add arrow icon based on the state
+    document.querySelector('.description>button>i').classList = state ? 'fa-solid fa-arrow-up' : 'fa-solid fa-arrow-down'
+
+    // Update the state
+    state = !state;
+};
+
+// Get the window width
+let w = window.innerWidth;
+
+// If the window width is greater than 768 pixels, show the full description
 if (w > 768) {
-    document.querySelector('.description>p').innerHTML = desc
+    document.querySelector('.description>p').innerHTML = desc;
 } else {
-    document.querySelector('.description>p').innerHTML = desc.substring(0, 201) + '...'
-    document.querySelector('.description>button').addEventListener("click", handleClick)
+    // If the window width is 768 pixels or less, show a truncated description and attach the click event to the button
+    document.querySelector('.description>p').innerHTML = desc.substring(0, 201) + '...';
+    document.querySelector('.description>button').addEventListener("click", handleClick);
 }
